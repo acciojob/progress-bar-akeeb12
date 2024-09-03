963fbf2bd45b7675765b3d7619f35d1c10105006
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-09-03 17:49:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
 //your JS code here. If required.
-const circles = document.querySelectortAll(".circle");
+const circles = document.querySelectorAll('.circle');
 
-const lines = document.querySelectorAll(".line");
+const lines = document.querySelectorAll('.line');
 
-const nextButton = document.querySelector("next");
-const prevButton = document.querySelector("prev");
+const nextButton = document.querySelector("#next");
+const prevButton = document.querySelector("#prev");
 
 let currentActive = 1;
 
 nextButton.addEventListener('click', () =>
 	{
 		currentActive++;	
+		updateUi();
 	});
 
 prevButton.addEventListener('click', () =>
 	{
 		currentActive--;	
 
-	});
+		updateUi();
+
+	})
 function updateUi() {
 	circles.forEach((circle, idx) =>
 		{
 			idx < currentActive ? 
 			circle.classList.add("active") :
-				cicrcle.classList.remove("active");
+				circle.classList.remove("active");
 		});
 	lines.forEach((line, idx) =>
 		{
 			idx < currentActive -1 ?
-				circle.classList.add("active") :
+				line.classList.add("active") :
 				line.classList.remove("active");
 		});
 
